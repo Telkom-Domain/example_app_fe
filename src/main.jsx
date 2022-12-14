0ce031f78e03d02ea069a9a30addf078486b5095
zakinadhif <zaki.nadhif45@gmail.com>
2022-12-14 13:04:52 +0700

    Fix access token errors

--- src/main.jsx
+++ src/main.jsx
@@ -9,19 +9,19 @@ import AuthProvider from "./contexts/auth-provider";
 import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <ChakraProvider>
-            <AuthProvider
-                domain="http://localhost:2005"
-                tenant="ijlal"
-                clientId="635a6d7a42a94f6e298869a0"
-                clientSecret="$2b$04$yMCGwaNoq6olOTUeKxB3bO5xFDz5XcSt0bD86jUBVa8vToDgxCxVq"
-                redirectUri={window.location.origin + "/notes"}
-                audience="http://localhost:8080"
-                scope="read:notes write:notes update:notes delete:notes"
-            >
-                <App />
-            </AuthProvider>
-        </ChakraProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <ChakraProvider>
+      <AuthProvider
+        domain="http://localhost:8000"
+        tenant="alpinemd"
+        clientId="6344f502f139bd25aebf1c10"
+        clientSecret="$2b$04$YzZ43eIxfjA1nlnjsszYXu7GLHxwk7hBtfreqBOU9m3eCSqZWDjmC"
+        redirectUri={window.location.origin + "/notes"}
+        audience="https://alpinemd.com/"
+        scope="read:notes write:notes update:notes delete:notes"
+      >
+        <App />
+      </AuthProvider>
+    </ChakraProvider>
+  </React.StrictMode>
 );
