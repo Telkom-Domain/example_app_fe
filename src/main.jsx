96b7c61fcfc1de820571f5e945dbb8512851e247
domain121314 <telkomdev.domain@gmail.com>
2023-03-17 19:00:31 +0700

    adding docker config

--- src/main.jsx
+++ src/main.jsx
@@ -12,12 +12,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
       <AuthProvider
-        domain="http://localhost:1000"
-        tenant="server-local"
-        clientId="63e4c142d7790d18a0942dc4"
-        clientSecret="$2b$04$wsIQODcadMaOtMHYXdFODuk6khAfGHVtzB6Te1CEojS7cNlx6BJHm"
+        domain="https://api-dev.domain-dev.site"
+        tenant="development"
+        clientId="64070cdfb96e6dcb31cb15c6"
+        clientSecret="$2b$04$4D9HaW3TJOYvVzAxvI8en.V7oQp5wFbI3wpmGm81fmcU04mNBw2iK"
         redirectUri={window.location.origin + "/notes"}
-        audience="http://localhost:1337"
+        audience="http://host.docker.internal:1337"
         scope="read:notes write:notes update:notes delete:notes"
       >
         <App />
